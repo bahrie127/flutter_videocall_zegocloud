38524d548ab91024a23386f45a32d6a02e320b85
Saiful Bahri <bahrie172@gmail.com>
2022-12-14 17:10:25 +0700

    Update zego_config.dart
    update appsign description

--- lib/zego_config.dart
+++ lib/zego_config.dart
@@ -1,4 +1,4 @@
 class ZegoConfig{
   static const int appId = 101010101;//changes with your appId
-  static const String appSign = '10101010101001';//changes with your appSign
+  static const String appSign = 'your-appSign-full';//changes with your appSign
 }
